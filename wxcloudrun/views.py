4547b12afb7fa334f1dab0104b5387e54673baf5
xuyuanxinxin <1846234482@qq.com>
2023-02-06 14:13:53 +0800

    update selenium script

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -5,27 +5,38 @@ from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter
 from wxcloudrun.model import Counters
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
-import time
 import requests
+from time import sleep
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-def download(url,filename):
+
+def download(url, filename):
     r = requests.get(url)
-    with open(filename+".mp4",mode="wb") as f:
+    with open(filename+".mp4", mode="wb") as f:
         f.write(r.content)
     print("download success")
 
 
 def main():
-    driver = webdriver.Chrome()
-    driver.get('https://www.douyin.com/video/7191967352978771260')
-    time.sleep(3)
-
-    video = driver.find_element(by=By.XPATH,value="//video/source")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument('lang=zh-CN.UTF-8')
+    options.add_argument("--no-sandbox")
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
+    service = Service(executable_path="/usr/bin/chromedriver")
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get("https://douyin.com")
+    sleep(5)
+    driver.save_screenshot("./tiktok.png")
+    video = driver.find_element(by=By.XPATH, value="//video/source")
     video_url = video.get_attribute("src")
-    download(video_url,"7191967352978771260")
+    download(video_url, "7191967352978771260")
     print(video)
+    print(video_url)
 
 
 @app.route('/')
